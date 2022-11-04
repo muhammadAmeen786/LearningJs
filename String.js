2f03fd4d0ea017348bddc6141755c2bf70bb3594
muhammadAmeen786 <mohdameeendar@gmail.com>
2022-11-05 02:00:22 +0530

    someMore String MEthods

--- String.js
+++ String.js
@@ -8,3 +8,22 @@ let str2 = str.slice(0,6);
 let str3 = str.slice(7,12);
 console.group(str2 + " " + str3);
 //OutPut ... this will print Hello coder.
+
+//strings are immutable in java. that means we can't do an assignment at any place of the string index.
+// which means we can't do any like this. str[4] = i; 
+//it will produce an error.
+
+//there are a lot of methods which are used in strings.
+str.push(3); //this will push an element at the end.
+str.pop(); /// this will remove the last element from the string.
+str.unshift() // this puts the element at the starting of the string.
+str.shift() // It removes an element from the starting of an string.
+str.slice(3,5) // this will trim the 3 to 5-1 index of the string and return that trimmed string.
+str.substr(3,4) // this will start from the third index and count till 4 characters and then return those characters.
+str.substring() // this also works the same like the substr.
+str.indexOf(s)  // this will return the given index of the given character.
+str.trim() // this method removes the leading and trailing spaces.
+str.concat(str2) // this method will concatenate str and str2
+str.includes('chr') // returns true or false.
+str.lastIndexOf(chr) // returns the last index at which the index is present.
+
---
+++ practice.js
@@ -0,0 +1,13 @@
+// let str = "hello pecoders welcome to learning js come";
+// console.log(str.lastIndexOf('we')); ..out put ==15
+
+// includes method in 
+
+let str = "hello pep";
+if(str.includes(" pp")){
+    console.log("Hello pepcoders");
+}
+else{
+    console.log("You are not registered");
+    
+}

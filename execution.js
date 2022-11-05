fe3b1dc2c74488d932b494573ec17d72f7f2b558
muhammadAmeen786 <mohdameeendar@gmail.com>
2022-11-06 03:45:07 +0530

    execution of js

---
+++ execution.js
@@ -0,0 +1,13 @@
+//learnt how the js code works behind the scenes.
+/* It is all about the execution context. And when a js file is run it has the global execution context 
+which holds all of the other functions and varibles at that time.
+Every function have its own local execution context which contains the variables and the code 
+that is to be run .
+once we are done with a particular function its execution context gets deleted at that particular 
+instant.
+
+Hoisting plays an important role in the execution of javascript code.
+functions are made available.
+and varibles are declared as undefined.
+
+/*/
